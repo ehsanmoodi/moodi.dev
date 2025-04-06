179aeb8150e86545d0cada353a8b01ad59f512bc
Ehsan Moodi <ehsanmoodi90@gmail.com>
2025-04-06 15:33:21 +0330

    update some projects

--- app/projects/project-data.tsx
+++ app/projects/project-data.tsx
@@ -1,8 +1,24 @@
 export interface Project {
   title: string;
-  year: number;
   description: string;
   url: string;
 }
 
-export const projects: Project[] = [];
+export const projects: Project[] = [
+  {
+    title: "Alleysa",
+    description:
+      "travel platform: Book Accommodations, Cars, And Local Guides.",
+    url: "https://alleysa.com",
+  },
+  {
+    title: "Ctatuscolana",
+    description: "Tour reservation system operating in Italy",
+    url: "https://ctatuscolana.it/",
+  },
+  {
+    title: "Honarchi",
+    description: "Shop for handmades",
+    url: "https://honarchi.com/",
+  },
+];
