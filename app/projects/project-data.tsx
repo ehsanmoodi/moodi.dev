ebd28b14fb049c7c9d5fe0016be8ebf410353f25
Ehsan Moodi <ehsanmoodi90@gmail.com>
2025-04-06 23:37:17 +0330

    add more projects

--- app/projects/project-data.tsx
+++ app/projects/project-data.tsx
@@ -21,4 +21,59 @@ export const projects: Project[] = [
     description: "Shop for handmades",
     url: "https://honarchi.com/",
   },
+  {
+    title: "Sade Zibast",
+    description: "Make a simple invitation card!",
+    url: "https://sade-zibast.vercel.app/",
+  },
+  {
+    title: "Giuliodoro",
+    description: "",
+    url: "https://giuliodoro.netlify.app/",
+  },
+  {
+    title: "Safaryaar",
+    description: "",
+    url: "https://safaryaar.com/",
+  },
+  {
+    title: "Pikoland",
+    description: "",
+    url: "https://pikoland.vercel.app/",
+  },
+  {
+    title: "Hanipers",
+    description: "",
+    url: "https://hanipers.vercel.app/",
+  },
+  {
+    title: "Tickers",
+    description: "",
+    url: "https://ticker.vercel.app/",
+  },
+  {
+    title: "Winkders",
+    description: "",
+    url: "https://winkers.vercel.app/",
+  },
+  {
+    title: "Fibora",
+    description: "",
+    url: "https://fibora.vercel.app/",
+  },
+  {
+    title: "Tmar Travel",
+    description: "",
+    url: "https://tmartravel.ir/",
+  },
+  {
+    title: "Bartarinha",
+    description: "",
+    url: "https://bartarinha.vercel.app/",
+  },
+  {
+    title: "Khabinja",
+    description: "",
+    url: "https://khabinja.com/",
+  },
 ];
